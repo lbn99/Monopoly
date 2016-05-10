d3ab8f3d58d0aa500a6f2ded8632c702c65581e8
Chandrakant <chandrakant@Chandrakants-MacBook-Pro.local>
2016-05-10 00:10:49 -0500

    this is the interface file for the property card with almost all the necessary functions included.

---
+++ src/gui/IPropertyCard.java
@@ -0,0 +1,58 @@
+public interface IPropertyCard
+{
+	//it sets the name of the card
+	public void setName(String s);
+
+	//it returns the name of the card
+	public String getName();
+
+	//it sets the cost of the card
+	public void setCost(Double d);
+
+	//it returns the cost of the card
+	public Double getCost();
+
+	//it sets the boolean available to true or false accordingly
+	public void setAvailable(boolean b);
+
+	//it returns the status of available
+	public boolean getAvailable();
+
+	//it sets the rents using a list/staticArray 
+	public void setRent(int[] r);
+
+	//it returns the list of the rents
+	public int[] getRent();
+
+	//it sets the color of the card
+	public void setColor(Color c);
+
+	//it returns the color of the card
+	public Color getColor();
+
+	//it sets the position cordinate (lower left corner)
+	public void setPosition(Pixel c);
+
+	//it returns the cordinate 
+	public Pixel getPosition();
+
+	//it sets the Y-size of the card
+	public void setY(int y);
+
+	//it sets the X-size of the card
+	public void setX(int x);
+
+	//it returns the Y size of the card
+	public int getY();
+
+	//it returns the X size of the card
+	public int getX();
+
+	//Lyndon, I'm not sure what this is going to do, so change it if it's wrong
+	public void setType(String s);
+
+	//it returns the type of the card
+	public String getType();
+
+
+}
