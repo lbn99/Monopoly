d9c52315b96d556dabe1feb5000cb1b51b39bcaf
LB Nguyen <ninjatuna2014@gmail.com>
2016-05-10 21:18:07 -0500

    added GraphicsPanel; main method in GameLauncher

---
+++ src/gui/GraphicsPanel.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class GraphicsPanel extends JPanel{
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		
+	}
+}
